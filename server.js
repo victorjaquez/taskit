ee67daac300fcf971f836069847757c57ab4c07f
victorjaquez <victorajaquez@gmail.com>
2018-03-24 12:29:48 -0700

    basic styling

--- server.js
+++ server.js
@@ -1,45 +1,29 @@
+// ==================== DEPENDENCIES ====================
 const express = require('express');
 const app =  express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
+// ===================== MIDDLEWARE =====================
+// body parser
 app.use(express.urlencoded({extended:false}));
+// method override
 app.use(methodOverride('_method'));
+// static files
+app.use(express.static('public'));
 
+// ==================== CONTROLLERS =====================
+// tasks
 const tasksController = require('./controllers/tasks.js');
 app.use('/tasks', tasksController);
 
-app.get('/tasks/seed', (req,res)=>{
-    Task.create([
-        {
-            description: 'Load moving truck',
-	        budget: '$150',
-	        location: 'Peoria',
-	        duedate: 'April 20th'
-        },
-        {
-            description: 'Paint house',
-	        budget: '$500',
-	        location: 'Peoria',
-	        duedate: 'April 21st'
-        },
-        {
-            description: 'Mount various flatscreens',
-	        budget: '$100',
-	        location: 'Scottsdale',
-	        duedate: 'April 5th'
-        },
-        {
-            description: 'Walk dogs',
-	        budget: '$50',
-	        location: 'Central Park',
-	        duedate: 'April 1st'
-        },
-    ], (err, data)=>{
-        res.redirect('/tasks');
-    });
+// ====================== GET ROUTES ====================
+// main home route
+app.get('/', (req, res)=>{
+    res.render('home.ejs');
 });
 
+// ============ MONGOOSE CONNECTION/HEROKU ===============
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/taskit';
 mongoose.connect(mongoURI);
 
@@ -47,6 +31,7 @@ mongoose.connection.once('open', ()=>{
     console.log('connected to mongod . . .');
 });
 
+// ====================== LISTENER ======================
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
     console.log('listening. . .');
